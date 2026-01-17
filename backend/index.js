9de2bea6bb0e7f370941d7d5f398f2c752ee73c6
Rishabh Jain <jainrishabh2610@gmail.com>
2026-01-17 12:29:34 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import UserRouter from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 
 
